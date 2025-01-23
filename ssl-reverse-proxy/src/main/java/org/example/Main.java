e0ccc7b77f57aa07eabfd1748dd8979e4e489f62
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-01-23 15:28:46 +0100

    Simple ReverseProxy

--- ssl-reverse-proxy/src/main/java/org/example/Main.java
+++ ssl-reverse-proxy/src/main/java/org/example/Main.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import org.eclipse.jetty.proxy.ProxyServlet;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.proxy.ProxyHandler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
   public static void main(String[] args) throws Exception {
     Server server = new Server();
 
@@ -19,17 +17,9 @@ public class Main {
     connector.setPort(8080); // Port for the proxy server
     server.addConnector(connector);
 
-    // Set up a servlet context
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-    server.setHandler(context);
-
-    // Add a ProxyServlet to forward requests to the target service
-    ServletHolder proxyServlet = new ServletHolder(ProxyServlet.class);
-    proxyServlet.setInitParameter("proxyTo", "http://localhost:9000"); // Target service URL
-    proxyServlet.setInitParameter("prefix", "/");
-
-    context.addServlet(proxyServlet, "/*");
+    // Create a ProxyHandler
+    ProxyHandler proxyHandler = new ProxyHandler.Reverse("http://localhost:8080", "http://localhost:9000");
+    server.setHandler(proxyHandler);
 
     // Start the server
     server.start();
