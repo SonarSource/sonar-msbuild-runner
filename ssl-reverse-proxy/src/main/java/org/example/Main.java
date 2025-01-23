8cc106587e3d1da9999e657a6a403b0990ef12d4
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-01-23 15:38:15 +0100

    Working SSL Reverse proxy

--- ssl-reverse-proxy/src/main/java/org/example/Main.java
+++ ssl-reverse-proxy/src/main/java/org/example/Main.java
@@ -1,24 +1,44 @@
 package org.example;
 
-import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
-import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.proxy.ProxyHandler;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     Server server = new Server();
+    int httpsPort = 9443;
 
-    // Create an HTTP connector
-    ServerConnector connector = new ServerConnector(server);
-    connector.setPort(8080); // Port for the proxy server
-    server.addConnector(connector);
+    HttpConfiguration httpConfig = new HttpConfiguration();
+    httpConfig.setSecureScheme("https");
+    httpConfig.setSecurePort(httpsPort);
+
+    // SSL Context Factory
+    SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+    sslContextFactory.setKeyStorePath("src/main/resources/keystore.p12");
+    sslContextFactory.setKeyStorePassword("your_password");
+    sslContextFactory.setKeyStoreType("PKCS12");
+
+    // SSL HTTP Configuration
+    HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+    httpsConfig.addCustomizer(new SecureRequestCustomizer()); // so that servlets can see the
+    // encryption details
+
+    // SSL Connector
+    ServerConnector sslConnector = new ServerConnector(server,
+      new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+      new HttpConnectionFactory(httpsConfig)); // <-- the argument you were missing
+    sslConnector.setPort(httpsPort);
+    server.addConnector(sslConnector);
 
     // Create a ProxyHandler
-    ProxyHandler proxyHandler = new ProxyHandler.Reverse("http://localhost:8080", "http://localhost:9000");
+    ProxyHandler proxyHandler = new ProxyHandler.Reverse("https://localhost:9443", "http://localhost:9000");
     server.setHandler(proxyHandler);
 
     // Start the server
